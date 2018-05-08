ebed7e421002b897c91de859ebbedf6f097295cb
Nicolas <nicolas.garcia@auth0.com>
2018-05-08 17:43:04 -0300

    Refactor worker

--- Bookmarks/worker/index.js
+++ Bookmarks/worker/index.js
@@ -1,35 +1,40 @@
-require('dotenv').config();
-const axios = require('axios');
-const {promisify} = require('util');
-const bookmarks = require('../lib/db/bookmarks.js');
+require("dotenv").config();
 
-const q = process.env.RABBIT_QUEUE_NAME;
-const open = require('amqplib').connect(process.env.RABBIT_QUEUE_URL);
+const axios = require("axios");
+const amqp = require("amqplib");
 
+const queueName = process.env.RABBIT_QUEUE_NAME;
+const rabbitURL = process.env.RABBIT_QUEUE_URL;
 
-// Consumer
-open
-  .then((conn) => {
-    return conn.createChannel()
-                .then((ch) => ch.assertQueue(q, {durable: true})
-                    .then(() => processQueue(ch))
-                    // .then(() => ch.close()))
-                ).catch(console.warn)
-                // .then(() => conn.close());
-  })
-  .catch(console.warn);
+const main = async (queueName, rabbitURL) => {
+  let conn;
 
+  try {
+    conn = await amqp.connect(rabbitURL);
 
-  const processQueue = (ch) => {
-    return ch.consume(q,
-        (msg) => processMsg(ch, msg),
-        //We're going to manually ack the item after proccessing it
-        { noAck: false });
+    const ch = await conn.createChannel();
+
+    // Check queue exists
+    await ch.assertQueue(queueName, { durable: true });
+
+    // Process queue items
+    await processQueue(ch, queueName);
+  } catch (e) {
+    conn.close();
+    console.error(e);
   }
+};
 
-  const processMsg = (ch, msg) => {
-    console.log("Proccessing a new message..");
-    console.log(msg);
-    //TODO: Check link and update item in db
-    ch.ack(msg);
-  }
+main(queueName, rabbitURL);
+
+const processQueue = (ch, queueName) =>
+  // We're going to manually ack the item after processing it
+  ch.consume(queueName, msg => processMsg(ch, msg), { noAck: false });
+
+const processMsg = (ch, msg) => {
+  console.log('Proccessing a new message..');
+
+  //TODO: Check link and update item in db
+
+  ch.ack(msg);
+};
