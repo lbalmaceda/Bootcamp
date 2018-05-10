3289f5051861b4b6cd99eef88b4ebb7165f6856a
Luciano Balmaceda <balmacedaluciano@gmail.com>
2018-05-10 17:42:58 -0300

    add link check logic

--- Bookmarks/worker/index.js
+++ Bookmarks/worker/index.js
@@ -1,6 +1,9 @@
 const config = require('../config');
 const axios = require("axios");
 const amqp = require("amqplib");
+const logger = require("../lib/log/logger.js");
+const bookmarks = require('../lib/db/bookmarks.js');
+const {promisify} = require('util');
 
 const { queueName, url } = config.rabbit;
 
@@ -19,20 +22,43 @@ const main = async (queueName, rabbitURL) => {
     await processQueue(ch, queueName);
   } catch (e) {
     conn.close();
-    console.error(e);
+    logger.error("Q Connection closed: " + e);
   }
 };
 
 main(queueName, url);
 
-const processQueue = (ch, queueName) =>
-  // We're going to manually ack the item after processing it
-  ch.consume(queueName, msg => processMsg(ch, msg), { noAck: false });
+const processQueue = (ch, queueName) => {
+  logger.info('Proccessing queue messages..');
+
+  return ch.consume(queueName, msg => processMsg(ch, msg),
+    { noAck: false }); // We're going to manually ack the item after processing it
+}
+  
 
 const processMsg = (ch, msg) => {
-  console.log('Proccessing a new message..');
+  let bookmark = JSON.parse(msg.content.toString());
+  logger.info('Proccessing a new message', bookmark);
 
-  //TODO: Check link and update item in db
+  return axios.get(bookmark.url, {
+      timeout: 2000 //TODO: Move to constant
+    })
+    .then((res) => {
+      let data = {
+        is_ok: res.status == 200,
+        id: bookmark.id
+      };
+      logger.info("About to update DB", data);
 
-  ch.ack(msg);
+      const updateStatus = promisify(bookmarks.updateStatus);
+      return updateStatus(data)
+        .then(() => {
+          ch.ack(msg);
+          logger.info("DB Write success. ACK sent.");
+        })
+        .catch((err) => logger.error("DB Error", err));
+    })
+    .catch((err) => {
+      logger.error("HTTP Error:", err);
+    });
 };
