8a3e00a623bc90c74d5919474e8585f9b32272c8
Luciano Balmaceda <balmacedaluciano@gmail.com>
2018-05-10 18:22:11 -0300

    update awk-sdk promise usage

--- Bookmarks/scheduler/metric.js
+++ Bookmarks/scheduler/metric.js
@@ -1,7 +1,6 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-2'});
 var cw = new AWS.CloudWatch({apiVersion: '2010-08-01'});
-const {promisify} = require('util');
 const logger = require('../lib/log/logger.js');
 
 const publishMetric = (queueSize) => {
@@ -16,10 +15,12 @@ const publishMetric = (queueSize) => {
     Namespace: 'WORKER'
   };
   
-  const putMetricData = promisify(cw.putMetricData);
-  return putMetricData(params)
+  cw.putMetricData(params).promise()
     .then(() => logger.info("Queue size metric updated"))
-    .catch((err) => logger.error("Failed to update Queue size metric", err));
+    //.catch((err) => logger.error("Failed to update Queue size metric", err));
+    .catch(console.warn)
 }
 
-module.exports = publishMetric;
+//module.exports = publishMetric;
+
+publishMetric(10);
